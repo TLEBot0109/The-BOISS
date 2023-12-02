7b5fa305fd757befbc2e2b7921a9ec0c6c2530b9
phamhungmaidinh <151750483+phamhungmaidinh@users.noreply.github.com>
2023-12-02 14:31:25 +0700

    Knight character

--- saved project/player.py
+++ saved project/player.py
@@ -1,9 +1,13 @@
 import pygame
-moving_sprites = pygame.sprite.Group()
+
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, dimensions, color, health, delay, controls, pre_time_moving):
+    def __init__(self, dimensions, color, health, delay, controls, 
+                 pre_time_moving,walking_way):
+        #0=right 1=left
         super().__init__()
+        self.moving_sprites = pygame.sprite.Group()
+        self.moving_sprites.add(self)
         self.x = dimensions[0]
         self.y = dimensions[1]
         self.wi = dimensions[2]
@@ -16,41 +20,54 @@ class Player(pygame.sprite.Sprite):
         self.delay=delay
         self.controls=controls
         self.pre_time_moving=pre_time_moving
-        self.sprites=[]
-        self.sprites.append(pygame.image.load('random1.png'))
-        self.sprites.append(pygame.image.load('random2.png'))
-        self.sprites.append(pygame.image.load('random3.png'))
-        self.sprites.append(pygame.image.load('random4.png'))
-        self.sprites.append(pygame.image.load('random5.png'))
-        self.sprites.append(pygame.image.load('random6.png'))
-        self.sprites.append(pygame.image.load('random7.png'))
-        self.sprites.append(pygame.image.load('random8.png'))
+        self.walking_way=walking_way
+        
+        
+        self.sprites=[[],[]]
+        self.sprites[0].append(pygame.image.load('knight_move_right1.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right2.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right3.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right4.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right5.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right6.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right7.png'))
+        self.sprites[0].append(pygame.image.load('knight_move_right8.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left1.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left2.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left3.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left4.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left5.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left6.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left7.png'))
+        self.sprites[1].append(pygame.image.load('knight_move_left8.png'))
+        
+        
         self.current_sprite = 0
-        self.image = self.sprites[self.current_sprite]
+        self.image = self.sprites[walking_way][self.current_sprite]
 
         self.rect = self.image.get_rect()
-        #self.rect.topleft = [x+50,y+50]
-        
-        moving_sprites.add(self)
+
     def update_image(self):
         self.current_sprite += 1
         if self.current_sprite >= len(self.sprites):
             self.current_sprite = 0
-        self.image = self.sprites[self.current_sprite]
+        self.image = self.sprites[self.walking_way][self.current_sprite]
     
     def draw(self, win):
         #pygame.draw.rect(win, self.color, self.rec)
-        moving_sprites.draw(win)
-        p=40
-        pygame.draw.line(win,(255,0,0),(self.x-25+p,self.y-30), 
-                         (self.x-25+50*(self.health/self.max_health)+p,self.y-30),3)
+        self.moving_sprites.draw(win)
+        p=-5
+        start=self.x-15+p
+        end=self.x+15+p
+        pygame.draw.line(win,(255,0,0),(start,self.y-25), 
+                         (start+(end-start+1)*(self.health/self.max_health),self.y-25),3)
         if(self.health < self.max_health):
             pygame.draw.line(win,(255,255,255),
-                         (self.x-25+50*(self.health/self.max_health)+1+p,self.y-30), 
-                         (self.x+25+p,self.y-30),3)
+                         (start+(end-start+1)*(self.health/self.max_health)+1,self.y-25), 
+                         (end,self.y-25),3)
     
     def delete_from_spirte(self):
-        moving_sprites.remove(self)
+        self.moving_sprites.remove(self)
     
     def Update(self):      
         #self.rec = (self.x - self.wi/2, self.y - self.he/2, self.wi, self.he)
@@ -60,45 +77,50 @@ class Player(pygame.sprite.Sprite):
     def move(self): 
         keys = pygame.key.get_pressed()
         change_image = False
-        max_screen=400
+        min_x=29
+        max_x=500-18
+        min_y=27
+        max_y=500-63
         if keys[self.controls[2]]:
             self.x -= self.vel
-            if self.x < 0:
-                self.x = 0
-            elif self.x > max_screen:
-                self.x = max_screen
+            if self.x < min_x:
+                self.x = min_x
+            elif self.x > max_x:
+                self.x = max_x
             change_image = True
+            self.walking_way=1
 
         if keys[self.controls[3]]:
             self.x += self.vel
-            if self.x < 0:
-                self.x = 0
-            elif self.x > max_screen:
-                self.x = max_screen
+            if self.x < min_x:
+                self.x = min_x
+            elif self.x > max_x:
+                self.x = max_x
             change_image = True
+            self.walking_way=0
 
         if keys[self.controls[0]]:
             self.y -= self.vel
-            if self.y < 0:
-                self.y = 0
-            elif self.y > max_screen:
-                self.y = max_screen
+            if self.y < min_y:
+                self.y = min_y
+            elif self.y > max_y:
+                self.y = max_y
             change_image = True
 
         if keys[self.controls[1]]:
             self.y += self.vel
-            if self.y < 0:
-                self.y = 0
-            elif self.y > max_screen:
-                self.y = max_screen
+            if self.y < min_y:
+                self.y = min_y
+            elif self.y > max_y:
+                self.y = max_y
             change_image = True
         
         if change_image == True:
             current_time= pygame.time.get_ticks()
-            if(current_time-self.pre_time_moving>500):
+            if(current_time-self.pre_time_moving>250):
                 self.update_image()
                 self.pre_time_moving=current_time
         else:
             self.current_sprite=0
-            self.image = self.sprites[self.current_sprite]
+            self.image = self.sprites[self.walking_way][self.current_sprite]
         self.Update()
