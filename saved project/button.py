4eaea85b424a822a14766fc1832a07beae7f9f09
phamhungmaidinh <151750483+phamhungmaidinh@users.noreply.github.com>
2023-12-02 23:40:24 +0700

    Update button.py

--- saved project/button.py
+++ saved project/button.py
@@ -1,20 +1,22 @@
 import pygame
 
 class Button():
-    def __init__(self, x, y, wi, he, color, contex):
+    def __init__(self, x, y, wi, he, color, ):
         self.x = x
         self.y = y
         self.wi = wi
         self.he = he
         self.color = color
-        self.contex = contex
+        self.rect=pygame.Rect(x,y,wi,he)
         
-        
+    def draw(self,win):
+        pygame.draw.rect(win,self.color,self.rect)
+    
     def get_clicked(self):
         mouse_button = pygame.mouse.get_pressed(num_buttons = 3)
         mouse_pos = pygame.mouse.get_pos()
-        if (mouse_button[0] and mouse_pos >= self.x - self.wi and mouse_pos <= self.x 
-            and mouse_pos >= self.y - self.he and mouse_pos <= self.y):
+        if (mouse_button[0] and 
+            self.rect.collidepoint(mouse_pos)):
             return True
         else: 
             return False 
